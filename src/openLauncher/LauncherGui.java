a68ca88671d08b48773e910a8db7deb9d59ec557
modmuss50 <m123world@gmail.com>
2014-12-13 09:08:33 +0000

    updates

--- src/openLauncher/LauncherGui.java
+++ src/openLauncher/LauncherGui.java
@@ -9,10 +9,21 @@ import java.awt.*;
 public class LauncherGui {
 	public JTabbedPane tabbedPane1;
 	public JPanel panel1;
+	public JTextField textField1;
+	public JPasswordField passwordField1;
+	public JButton button1;
+	public JCheckBox checkBox1;
+	public JComboBox comboBox1;
+	public JTextField textField2;
+	public JTextPane textPane1;
 
 	public static void main(String[] args) {
+		LauncherGui launcherGui = new LauncherGui();
+		launcherGui.comboBox1.addItem("Creeper Host");
+		launcherGui.comboBox1.addItem("Creeper Host 2");
+		launcherGui.textPane1.setText("This is some test news it does not load it from the net at the moment. This is so i can check some things and take some screen shots of it in use.");
 		JFrame frame = new JFrame("LauncherGui");
-		frame.setContentPane(new LauncherGui().panel1);
+		frame.setContentPane(launcherGui.panel1);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.pack();
 		frame.setVisible(true);
