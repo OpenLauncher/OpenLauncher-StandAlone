f8347db27306862bd5e37dbec04981d17700d9fd
modmuss50 <m123world@gmail.com>
2015-04-27 20:38:40 +0100

    Added zip creator

--- build.py
+++ build.py
@@ -6,32 +6,41 @@ import os
 import errno
 import sys
 import shutil
+from distutils.dir_util import copy_tree
+import zipfile
 
 baseDir = os.path.dirname(os.path.abspath(sys.argv[0])) + "/buildZip/"
+buildDir = os.path.dirname(os.path.abspath(sys.argv[0])) + "/builds/"
+lwjglDir = os.path.dirname(os.path.abspath(sys.argv[0]))
 runDir = os.path.dirname(os.path.abspath(sys.argv[0])) + "/"
+jarVersion = "0.0.0"
+if(os.environ.get('BUILD_NUMBER') != None):
+	jarVersion = "0.0." + os.environ.get('BUILD_NUMBER')
+
 jarName = "OpenLauncher-lwjgl-test-main.jar"
 
 
-def buildMac():
-	print("Building OSX version")
-	filename =  baseDir + "OSX/"
-	make_sure_path_exists(filename)
-	#copys the jar file into the dir ready to make the zip
-	shutil.copyfile(runDir + "build/libs/" + jarName, filename + jarName);
+def build(version):
+	print("Building " + version + " version")
+	filename =  baseDir + version + "/"
+	if(os.path.exists(filename)):
+			print("--Removing " + version)
+			shutil.rmtree(filename);
 
-def buildWin():
-	print("Building Windows version")	
-	filename = baseDir + "Windows/"
 	make_sure_path_exists(filename)
 	#copys the jar file into the dir ready to make the zip
-	shutil.copyfile(runDir + "build/libs/" + jarName, filename + jarName);
-
-def buildLinux():
-	print("Building Linux version")
-	filename = baseDir + "Linux/"
-	make_sure_path_exists(filename)
-	#copys the jar file into the dir ready to make the zip
-	shutil.copyfile(runDir + "build/libs/" + jarName, filename + jarName);
+	print("--Copying jar file " + version)
+	shutil.copy2(runDir + "build/libs/" + jarName, filename + jarName);
+	#copy libs
+	make_sure_path_exists(filename + "OpenLauncher/")
+	print("--Copying lwjgl libs " + version)
+	copy_tree(lwjglDir + "/lwjgl/jar", filename + "OpenLauncher/jar");
+	#copy natives
+	print("--Copying lwjgl natives " + version)
+	copy_tree(lwjglDir + "/lwjgl/native/" + version, filename + "OpenLauncher/natives");
+	#build zipFile
+	print("--Creating zip file " + version)
+	shutil.make_archive(buildDir + 'OpenLauncher-' + version + '-' + jarVersion +'.zip', 'zip', filename)
 
 
 def make_sure_path_exists(path):
@@ -39,9 +48,17 @@ def make_sure_path_exists(path):
         os.makedirs(path)
     except OSError as exception:
         if exception.errno != errno.EEXIST:
-            raise
+            raise      
+
+def zipdir(path, zip):
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            zip.write(os.path.join(root, file))              
 
 
-buildMac();
-buildWin();
-buildLinux();
+build("macosx");
+build("windows");
+build("linux");
+build("freebsd");
+build("openbsd");
+build("solaris");
